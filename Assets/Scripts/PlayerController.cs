0fa9d6e568bba08586a0e90182f53fa103a024c9
Max Davidse <m.davidse@rubicon.nl>
2023-07-27 21:15:40 +0200

    Added jump functionality to our player, plus added a ground check to make sure our player can't keep jumping. Added a Layer for the ground to do this.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _rigidBody;
+    [SerializeField] Transform _groundPoint;
     [SerializeField] float _moveSpeed = 8;
     [SerializeField] float _jumpForce = 20;
+    [SerializeField] LayerMask _whatIsGround;
+    [SerializeField] float _groundCheckRadius = .2f;
+    bool _isOnGround;
+    
 
     void Start()
     {
@@ -14,5 +19,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * _moveSpeed, _rigidBody.velocity.y);
+
+        _isOnGround = Physics2D.OverlapCircle(_groundPoint.position, _groundCheckRadius, _whatIsGround);
+        
+        if (Input.GetButtonDown("Jump") && _isOnGround)
+        {
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpForce);
+        }
     }
 }
